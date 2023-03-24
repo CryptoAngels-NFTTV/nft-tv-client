1cd61f4ab37d465cd0c82605eb7d54b13ceaa204
abdou abdou <abdou-elboursoumi@outlook.fr>
2023-03-24 18:03:51 +0100

    added authentification :tada:

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 
 export default function Header(){
 
     const [isActive, setActive] = useState(false);
+
+    const { loginWithRedirect } = useAuth0();
 
     const handleToggle = () => {
         setActive(!isActive);
@@ -23,8 +27,8 @@ export default function Header(){
                     <a href="#">CryptoAngels</a>
                     <a href="#">NFTTV</a>
                     <div className={`logins${isActive ? " is-active" : ""}`}>
-                        <button className="btn-black">Log in</button>
-                        <button>Register</button>
+                        <button className="btn-black" onClick={() => loginWithRedirect()}>Log in</button>
+                        <button onClick={() => loginWithRedirect()}>Register</button>
                     </div>
                 </div>
                 <button className={`hamburger${isActive ? " is-active" : ""}`} onClick={handleToggle}>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import './style.css'
 import './styles/playlist.css'
 import './styles/earn.css'
@@ -6,8 +8,19 @@ import './styles/team.css'
 import './styles/footer.css'
 import Home from './Home.jsx'
 
+
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <Home />
+
+    <Auth0Provider
+        domain="dev-8cfuc6dy5gmdhotp.us.auth0.com"
+        clientId="c5x6sPBshNWpA5EcpLf7LsWACpbFhlWa"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+
+        <Home />
+    </Auth0Provider>
 )
